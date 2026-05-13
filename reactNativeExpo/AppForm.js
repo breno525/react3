a01f7f898dc0914f467f1d16485afc8a81751c46
breno525 <breno.wesley525@gmail.com>
2026-05-13 23:35:27 +0000

    130526A

--- reactNativeExpo/AppForm.js
+++ reactNativeExpo/AppForm.js
@@ -1,3 +1,27 @@
 import { StatusBar} from 'expo-status-bar';
 import React from 'react';
-importo
+import { TextInput } from 'react-native-web';
+
+export default function AppForm() {
+    return (
+        <View style={styles.container}>
+        <Text style={style.title}>Item para comprar</Text>
+        <View style={styles.inputContainer}>
+            <TextInput
+            style={styles.input}
+            placeholder="O que está faltando em casa?"
+            clearButtonMode="always" />
+            <TextInput
+            style={styles.input}
+            placeholder="Digite a quantidade"
+            keyboardType={'numeric'}
+            clearButtonMode="always" />
+            <Touchable0pacity style={styles.buttonText}>
+                <Text style={styles.buttonText}>Salvar</Text>
+                </Touchable0pacity>
+         </View>
+         <StatusBar style="light" />
+         </View>
+    );
+}
+    
